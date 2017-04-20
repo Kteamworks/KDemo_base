325915ee42d14757df0b4dabd6a67eb7d49de9ae
PavithraSrinivas <pavithras@kavaii.com>
2017-04-20 13:24:51 +0530

    Vitals to ROS

--- demo/interface/forms/vitals/save.php
+++ demo/interface/forms/vitals/save.php
@@ -1,9 +1,11 @@
 <?php
 include_once("../../globals.php");
 include_once("$srcdir/api.inc");
-
+$url="../../patient_file/encounter/load_form.php?formname=ros";
+header('Location: '.$url);
 require ("C_FormVitals.class.php");
 $c = new C_FormVitals();
 echo $c->default_action_process($_POST);
 @formJump();
+
 ?>
