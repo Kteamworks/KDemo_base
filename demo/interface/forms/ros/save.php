21c46f108a33afd360b488884b647d1141fe17d1
PavithraSrinivas <pavithras@kavaii.com>
2017-04-20 13:34:52 +0530

    ROS to Lab

--- demo/interface/forms/ros/save.php
+++ demo/interface/forms/ros/save.php
@@ -1,7 +1,8 @@
 <?php
 include_once("../../globals.php");
 include_once("$srcdir/api.inc");
-
+$url="../../patient_file/encounter/load_form.php?formname=procedure_order";
+header('Location: '.$url);
 require ("C_FormROS.class.php");
 $c = new C_FormROS();
 echo $c->default_action_process($_POST);
