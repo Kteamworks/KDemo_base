21e27a16b33005f71ad65ddc08f6c196c98c934a
sada059 <sada059@gmail.com>
2018-02-07 13:32:54 +0530

    changed to global

--- demo/interface/forms/newpatient/mysql.php
+++ demo/interface/forms/newpatient/mysql.php
@@ -1,21 +1,15 @@
 <?php 
 
-//CREDENTIALS FOR DB
-define ('DBSERVER', 'localhost');
-define ('DBUSER', 'root');
-define ('DBPASS','');
-define ('DBNAME','mangoo');
-
-//LET'S INITIATE CONNECT TO DB
-$connection = mysql_connect(DBSERVER, DBUSER, DBPASS) or die("Can't connect to server. Please check credentials and try again");
-$result = mysql_select_db(DBNAME) or die("Can't select database. Please check DB name and try again");
+ require_once("../../globals.php");
+ require_once("$srcdir/patient.inc");
+ 
 
 //CREATE QUERY TO DB AND PUT RECEIVED DATA INTO ASSOCIATIVE ARRAY
 if (isset($_REQUEST['query'])) {
     $query = $_REQUEST['query'];
-    $sql = mysql_query ("SELECT genericname1, fname FROM patient_data WHERE genericname1 LIKE '%{$query}%' OR fname LIKE '%{$query}%'");
+    $sql = sqlStatement ("SELECT genericname1, fname FROM patient_data WHERE genericname1 LIKE '%{$query}%' OR fname LIKE '%{$query}%'");
 	$array = array();
-    while ($row = mysql_fetch_array($sql)) {
+    while ($row = sqlFetchArray($sql)) {
         $array[] = array (
             'label' => $row['genericname1'].', '.$row['fname'],
             'value' => $row['fname'],
