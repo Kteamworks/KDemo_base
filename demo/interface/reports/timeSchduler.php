70cbb67393968eec6cbd251b6e8ef009c3d10193
sada059 <sada059@gmail.com>
2018-02-12 15:45:57 +0530

    search select services and medicines

--- demo/interface/reports/timeSchduler.php
+++ demo/interface/reports/timeSchduler.php
@@ -247,13 +247,13 @@ if(isset($_POST['submit'])){
 <head>
   <title>Bootstrap Example</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.10.0/css/bootstrap-select.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.10.0/js/bootstrap-select.min.js"></script>
   <script>
     function printContent(el){
 	var restorePage = document.body.innerHTML;
@@ -547,7 +547,7 @@ $(window).load(function() {
 	  
 	 
 
-        <select class="form-control" name="medicine" id='reqMed'>
+        <select class="selectpicker" data-show-subtext="true" data-live-search="true" name="medicine" id='reqMed'>
 		<option value="">Select</option>
 		 <?php
           $medList = sqlStatement("select * from drugs order by name");
@@ -569,7 +569,7 @@ $(window).load(function() {
 	  
 	 
 
-        <select class="form-control" name="specialService" id='reqSpe'>
+        <select class="selectpicker" data-show-subtext="true" data-live-search="true" name="specialService" id='reqSpe'>
 		<option value="">Select</option>
 		 <?php
           $serList = sqlStatement("SELECT id,code,code_text,code_type,service_id FROM codes WHERE active=1 AND code_type=6 ORDER BY id,code");
