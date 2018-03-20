5c91768b300a24763b3d7858aa28e68b2dc7f52d
sada059 <sada059@gmail.com>
2018-03-20 14:04:36 +0530

    normal alerts working
    please check the validation now

--- demo/interface/forms/newpatient/common.php
+++ demo/interface/forms/newpatient/common.php
@@ -980,7 +980,7 @@ function getAge(dateString) {
 										
 
 <script language="javascript">
-alert = function() {};
+//alert = function() {};
 /* required for popup calendar 
 Calendar.setup({inputField:"form_date", ifFormat:"%Y-%m-%d", button:"img_form_date"});
 Calendar.setup({inputField:"form_onset_date", ifFormat:"%Y-%m-%d", button:"img_form_onset_date"});
