afb353627b61a5684d6ef9ee2c83c852cf9c6f5a
PavithraSrinivas <pavithras@kavaii.com>
2018-03-23 12:17:48 +0530

    Revert "dissable option payment against when patient is not insurence patient"
    This reverts commit c9bb538265290635f55c34c1d088196ff054342e.

--- demo/interface/patient_file/front_payment.php
+++ demo/interface/patient_file/front_payment.php
@@ -1481,7 +1481,6 @@ function make_hide_row()
  {
   document.getElementById('table_display').style.display="none";
   document.getElementById('table_display_prepayment').style.display="";
-  jQuery("#form_towards").val('Male');
  }
 function make_self()
  {
