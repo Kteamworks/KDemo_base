97eb87bffb5cc4a4dada0f4f74ad2b91050a0593
PavithraSrinivas <pavithras@kavaii.com>
2018-03-27 16:36:26 +0530

    Bed management form coloumns should be changed

--- demo/interface/forms/Bed_Summary/getuser.php
+++ demo/interface/forms/Bed_Summary/getuser.php
@@ -1,4 +1,14 @@
-<!DOCTYPE html>
+<?php include_once("../../globals.php");
+include_once("$srcdir/api.inc");
+include_once("$srcdir/forms.inc");
+include_once("$srcdir/encounter.inc");
+require_once("$srcdir/formdata.inc.php");
+require_once("$srcdir/patient.inc");
+require_once("$srcdir/acl.inc");
+require_once("$srcdir/options.inc.php");
+require_once("$srcdir/formatting.inc.php");
+require_once("$srcdir/erx_javascript.inc.php");
+?>
 <html>
 <head>
 <style>
@@ -20,21 +30,23 @@ th {text-align: left;}
 <?php
 $q = intval($_GET['q']);
 
-$con = mysqli_connect('localhost','root','admin123','greencity');
+/*$con = mysqli_connect('localhost','root','','mangoo');
 if (!$con) {
     die('Could not connect: ' . mysqli_error($con));
 }
 $wards="wards";
-mysqli_select_db($con,"greencity");
+mysqli_select_db($con,"mangoo");
 $sql="SELECT * FROM list_options WHERE list_id='".General."' AND is_default = '".$q."'";
-$result = mysqli_query($con,$sql);
-
+$result = sqlStatement($con,$sql);*/
+$sql="SELECT * FROM list_options WHERE list_id='".General."' AND is_default = '".$q."'";
 if ($q=='1')
 {
+//$sql="SELECT * FROM list_options WHERE list_id='".General."' AND is_default = '".$q."'";
 echo "<table>
 <tr>
-<th>Patient</th>
-<th>Encounter</th>
+<th>Patient Name</th>
+<th>Patient ID</th>
+<th>Visit ID</th>
 <th>Department</th>
 <th>Ward</th>
 <th>Bed</th>
@@ -45,15 +57,17 @@ echo "<table>
 
 </tr>";
     $adm='discharge';
-	$sql2="select * from t_form_admit where status='".$adm."'";
-	$result2=mysqli_query($con,$sql2);
+	$sql2="select a.diagnosis,b.pid,a.admit_to_bed,a.admit_to_ward,a.encounter,concat(b.fname,' ',b.lname) name,a.admit_date,b.genericname1 from t_form_admit a,patient_data b where a.status='".$adm."' and a.pid=b.pid";
+	$sql3="select * from t_form_admit  where status='".$adm."'";
+	$result2=sqlStatement($sql2);
 
-	while($row2=mysqli_fetch_array($result2))
+	while($row2=sqlFetchArray($result2))
 	{
 	
 	
     echo "<tr>";
-	echo "<td>" . $row2['client_name'] . "</td>";
+	echo "<td>" . $row2['name'] . "</td>";
+	echo "<td>" . $row2['genericname1'] . "</td>";
 	echo "<td>" . $row2['encounter'] . "</td>";
 	echo "<td>" . "" . "</td>";
 	echo "<td>" . $row2['admit_to_ward'] . "</td>";
@@ -69,19 +83,19 @@ else if($q=='0')
 {
 echo "<table>";
 echo "<tr>
-<th>Patient</th>
-<th>MED NO.</th>
-<th>Encounter</th>
+<th>Patient Name</th>
+<th>Patient ID</th>
+<th>Visit ID</th>
 <th>Ward</th>
 <th>Bed</th>
 <th>Admit Date</th>
 </tr>";
 
     $adm='admit';
-	$sql2="select a.pid,a.admit_to_bed,a.admit_to_ward,a.encounter,a.client_name,a.admit_date,b.genericname1 from t_form_admit a,patient_data b where a.status='".$adm."' and a.pid=b.pid order by a.admit_to_ward ";
-	$result2=mysqli_query($con,$sql2);
+	$sql2="select a.pid,a.admit_to_bed,a.admit_to_ward,a.encounter,concat(b.fname,' ',b.lname) name,a.admit_date,b.genericname1 from t_form_admit a,patient_data b where a.status='".$adm."' and a.pid=b.pid order by a.admit_to_ward ";
+	$result2=sqlStatement($sql2);
 	
-while($row2=mysqli_fetch_array($result2))
+while($row2=sqlFetchArray($result2))
 	{
 	$rdm=$row2['admit_to_ward'];
 	$rdm2=$row2['admit_to_bed'];
@@ -93,7 +107,7 @@ while($row2=mysqli_fetch_array($result2))
 	echo  "<input type=\"hidden\" name=\"admit_to_ward\" value=\"$rdm\" readonly></input>";
     echo "<input type=\"hidden\" name=\"admit_to_bed\" value=\"$rdm2\" readonly></input>";
     echo "<tr>";
-	echo "<td>" . $row2['client_name'] . "</td>";
+	echo "<td>" . $row2['name'] . "</td>";
 	echo "<td>" . $row2['genericname1'] . "</td>";
 	echo "<td>" . $row2['encounter'] . "</td>";
 	//echo "<td>" . "" . "</td>";
@@ -128,9 +142,9 @@ echo "<table>
 </tr>";
     $adm='discharge';
 	$sql2="select * from t_form_admit where status='".$adm."'";
-	$result2=mysqli_query($con,$sql2);
+	$result2=sqlStatement($sql2);
 
-	while($row2=mysqli_fetch_array($result2))
+	while($row2=sqlFetchArray($result2))
 	{
 	
 	$e=$row2['encounter'];
@@ -181,9 +195,9 @@ echo "<table>
 </tr>";
     $adm='admit';
 	$sql2="select * from t_form_admit where status='".$adm."'";
-	$result2=mysqli_query($con,$sql2);
+	$result2=sqlStatement($sql2);
 
-	while($row2=mysqli_fetch_array($result2))
+	while($row2=sqlFetchArray($result2))
 	{
 	
 	$rdm=$row2['admit_to_ward'];
@@ -217,9 +231,6 @@ echo "<input type=\"hidden\" name=\"fd\" value=\"Final Admit\"></input>";
 
 }
 
-
-
-mysqli_close($con);
 ?>
 
 </body>
